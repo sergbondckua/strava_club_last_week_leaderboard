060314d39f062f51d99be07d2518a9c888a2e4d9
sergbondckua <sergninja@gmail.com>
2022-12-19 23:09:10 +0200

    create full release

--- main.py
+++ main.py
@@ -8,9 +8,12 @@ from sender import SenderTelegram
 
 def main():
     """Main function"""
+    # Auth on Strava and get leaders club
     my_strava = Strava(email=os.getenv("LOGIN"), password=os.getenv("PASSWD"))
     rank_in_club = my_strava.get_last_week_leaders(club_id=582642)
+    # Create posters of leaders
     Poster(rank_in_club).create_poster()
+    # Sending posters via Telegram
     send = SenderTelegram(token_bot=os.getenv("TOKEN_BOT"))
     send.telegram_send(os.getenv("CHAT_ID"),
                        language="ukr",
