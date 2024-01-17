1b9dd338c42eacac923eb7602e207c07a8ef95d4
sergbondckua <sergninja@gmail.com>
2024-01-17 14:56:20 +0200

    edit backgrnd from season

--- main.py
+++ main.py
@@ -4,28 +4,26 @@ from datetime import datetime, timedelta
 import config
 from parse import StravaLeaderboardRetriever
 from poster import PosterAthletesCollector
+from poster_maker.creator import AthleteRankPosterGenerator
 from tg_sender import TelegramSender
 
 
-async def get_season_config(poster_generator, month=None):
+async def get_season_config(poster_generator: AthleteRankPosterGenerator):
     """Set season-specific configurations for the poster generator."""
+
+    month: int = (datetime.now() - timedelta(weeks=1)).month
 
     background_dir = (
         config.BASE_DIR / "poster_maker/resources/images/poster_bgrnd"
     )
     background_images = {
-        "winter": background_dir / "winter.jpg",
-        "spring": background_dir / "spring.jpg",
-        "summer": background_dir / "summer.jpg",
-        "autumn": background_dir / "autumn.jpg",
-        "other": background_dir / "other.jpg",
-        "other_winter": background_dir / "other_winter.jpg",
+        "winter": "winter.jpg",
+        "spring": "spring.jpg",
+        "summer": "summer.jpg",
+        "autumn": "autumn.jpg",
+        "other": "other.jpg",
+        "other_winter": "other_winter.jpg",
     }
-
-    # Checking that the month is within the acceptable range (1 to 12)
-    if month not in range(1, 13):
-        print("Month must be between 1 and 12")
-        return None
 
     # Dictionary with information about seasons and corresponding settings
     season_configs = {
@@ -42,19 +40,18 @@ async def get_season_config(poster_generator, month=None):
     # Determining the season based on the current month
     for months, settings in season_configs.items():
         if month in months:
-            poster_generator.BACKGROUND_IMAGE_PATH = background_images[
-                settings[0]
-            ]
-            poster_generator.BACKGROUND_2_IMAGE_PATH = background_images[
-                settings[1]
-            ]
+            poster_generator.BACKGROUND_IMAGE_PATH = (
+                background_dir / background_images[settings[0]]
+            )
+            poster_generator.BACKGROUND_2_IMAGE_PATH = (
+                background_dir / background_images[settings[1]]
+            )
             poster_generator.AVATARS_TOP3_POSITIONS = settings[2]
             break
 
 
 async def main():
     """Main function"""
-    current_month = (datetime.now() - timedelta(weeks=1)).month
 
     # Get Athletes data
     strava = StravaLeaderboardRetriever(
@@ -67,7 +64,7 @@ async def main():
     # Generate and save posters
     poster = PosterAthletesCollector(athletes_rank)
     # Apply settings according to the seasons
-    await get_season_config(poster.poster_generator, current_month)
+    await get_season_config(poster.poster_generator)
     await poster.create_and_save_posters()
 
     # Sending posters via Telegram
